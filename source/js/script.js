263935a63ccc3b90b875d7a76054ad20eaf5d242
Svetlana-Orlova <Svetlana-Orlova@github.com>
2021-07-21 00:59:54 +0300

    Compliance with PixelPerfect

--- source/js/script.js
+++ source/js/script.js
@@ -1,29 +1,63 @@
 "use strict";
+const body = document.body;
 const navMain = document.querySelector(".main-nav");
 const navToggle = document.querySelector(".main-nav__toggle");
 const anchors = document.querySelectorAll(".main-nav__item a");
+const inputs = document.querySelectorAll("input");
+const links = document.querySelectorAll(".page-main a");
+const buttons = document.querySelectorAll(".page-main button");
 
 navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener("click", function() {
+navToggle.addEventListener("click", () => {
   if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
+    openMenu();
   } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
+    closeMenu();
   }
 });
 
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function (e) {
-  e.preventDefault()
-
-  const sectionID = anchor.getAttribute("href").substr(1)
-
-  document.getElementById(sectionID).scrollIntoView({
-      behavior: "smooth",
-      section: "start"
-    })
-  })
+const openMenu = () => {
+  navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    body.classList.add("fixed");
+    disableItems(inputs);
+    disableItems(links);
+    disableItems(buttons);
 }
+
+const closeMenu = () => {
+  navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+    body.classList.remove("fixed");
+    enableItems(inputs);
+    enableItems(links);
+    enableItems(buttons);
+}
+
+const disableItems = (items) => {
+  items.forEach((item) => {
+    item.setAttribute(`disabled`, true);
+  });
+};
+
+const enableItems = (items) => {
+  items.forEach((item) => {
+    item.removeAttribute(`disabled`);
+  });
+};
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const sectionID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(sectionID).scrollIntoView({
+        behavior: "smooth",
+        section: "start"
+      });
+
+    closeMenu();
+  });
+};

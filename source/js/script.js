f2dc622dc50aaf364849a5ab8905f324e77301cf
Svetlana-Orlova <Svetlana-Orlova@github.com>
2021-07-22 13:10:28 +0300

    Edits in js

--- source/js/script.js
+++ source/js/script.js
@@ -7,60 +7,61 @@
   const links = document.querySelectorAll('.page-main a');
   const buttons = document.querySelectorAll('.page-main button');
 
-  if(document.getElementById('main-nav')&&document.getElementById('nav-toggle')&&document.getElementById('anchors')) {
+  if(navMain&&navToggle&&anchors&&inputs&&links&&buttons) {
     navMain.classList.remove('main-nav--nojs');
-  }
 
-  const disableItems = (items) => {
-    items.forEach((item) => {
-      item.setAttribute('disabled', true);
-    });
-  };
 
-  const enableItems = (items) => {
-    items.forEach((item) => {
-      item.removeAttribute('disabled');
-    });
-  };
-
-  const openMenu = () => {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    body.classList.add('fixed');
-    disableItems(inputs);
-    disableItems(links);
-    disableItems(buttons);
-  };
-
-  const closeMenu = () => {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    body.classList.remove('fixed');
-    enableItems(inputs);
-    enableItems(links);
-    enableItems(buttons);
-  };
-
-  navToggle.addEventListener('click', () => {
-    if (navMain.classList.contains('main-nav--closed')) {
-      openMenu();
-    } else {
-      closeMenu();
-    }
-  });
-
-  anchors.forEach((anchor) => {
-    anchor.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      const sectionID = anchor.getAttribute('href').substr(1);
-
-      document.getElementById(sectionID).scrollIntoView({
-        behavior: 'smooth',
-        section: 'start',
+    const disableItems = (items) => {
+      items.forEach((item) => {
+        item.setAttribute('disabled', true);
       });
+    };
 
-      closeMenu();
+    const enableItems = (items) => {
+      items.forEach((item) => {
+        item.removeAttribute('disabled');
+      });
+    };
+
+    const openMenu = () => {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      body.classList.add('fixed');
+      disableItems(inputs);
+      disableItems(links);
+      disableItems(buttons);
+    };
+
+    const closeMenu = () => {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      body.classList.remove('fixed');
+      enableItems(inputs);
+      enableItems(links);
+      enableItems(buttons);
+    };
+
+    navToggle.addEventListener('click', () => {
+      if (navMain.classList.contains('main-nav--closed')) {
+        openMenu();
+      } else {
+        closeMenu();
+      }
     });
-  });
+
+    anchors.forEach((anchor) => {
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const sectionID = anchor.getAttribute('href').substr(1);
+
+        document.getElementById(sectionID).scrollIntoView({
+          behavior: 'smooth',
+          section: 'start',
+        });
+
+        closeMenu();
+      });
+    });
+  }
 })();
